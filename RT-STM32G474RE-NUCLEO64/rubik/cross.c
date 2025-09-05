de50e211b7187673aace58067a918b83f260c71c
domenico.cimmino <domenico.cimmino>
2025-09-05 14:27:18 +0200

    Fix for

--- RT-STM32G474RE-NUCLEO64/rubik/cross.c
+++ RT-STM32G474RE-NUCLEO64/rubik/cross.c
@@ -25,7 +25,7 @@ int checkCross(Cube* cube){
 void solveCrossEdge(Cube* cube){
   for (int side=0; side<6; ++side) {
     for (int edge = 1; edge<9; edge+=2) {
-      if (cube->cubies[edge][side] == 0) {
+      if (cube->cubies[side][edge] == 0) {
         //Solve edge
 
         switch (side) {
